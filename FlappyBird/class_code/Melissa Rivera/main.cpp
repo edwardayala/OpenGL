8c234d55561d211b1bbeaa1fac19e8a13a5bd5d2
edwardayala <eaayala@txwes.edu>
2020-02-27 13:18:56 -0600

    added another class work

--- FlappyBird/class_code/Melissa Rivera/main.cpp
+++ FlappyBird/class_code/Melissa Rivera/main.cpp
@@ -1,4 +1,11 @@
-#include <GL/glut.h>
+//
+//  main.cpp
+//  flappy-bird
+//
+//  Created by Melissa  Rivera  on 2/5/20.
+//  Copyright © 2020 Melissa  Rivera . All rights reserved.
+//
+#include <GLUT/GLUT.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -8,8 +15,6 @@ using namespace std;
 //======================================================
 // CONSTANTS
 //======================================================
-// PI
-long double const PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899863;
 // Square size
 #define size 50.0
 
@@ -17,8 +22,8 @@ long double const PI = 3.1415926535897932384626433832795028841971693993751058209
 // GLOBAL VARIABLES WRITTEN TO BY reshapeCallBack( )
 //======================================================
 // Window size
-int w_height = 800;
-int w_width = 800;
+int w_height = 500;
+int w_width = 500;
 
 //======================================================
 // GLOBAL VARIABLES WRITTEN TO BY displayCallBack( )
@@ -28,35 +33,58 @@ int display_count = 0; //keeps count of invocations
 //======================================================
 // GLOBAL VARIABLES WRITTEN TO BY keyboardCallBack( )
 //======================================================
-float theta = 0.0; // Rotation angle of bird 
+float theta = 0.0; // Rotation angle of bird
 int timer_period_in_ms = 100; // time between timer callbacks in milliseconds
 
 //====================================================================
 // GLOBAL VARIABLES WRITTEN TO BY motionCallBack( )
 //====================================================================
 int bird_r=20;
-float bird_x=40; 
+float bird_x=40;
 float bird_y=40;; // position of bird
-double bird_vx = 0; 
+double bird_vx = 0;
 double bird_vy = 0;
-double bird_ax = 0; 
+
+
+float building1_x =100;
+float building2_x =100;
+float building3_x =100;
+
+
+//accelertation
+double bird_ax = 0;
 double bird_ay = 0;
-int bird_dx = 1; // incremental change inbird_x
+int bird_dx = 5; // incremental change inbird_x
 float dt = 0.15; //0.05;
 double impulse = 0;
+
+
+                    //for background
+
+double bg_ax = 0;
+double bg_ay = 0;
+int bg_dx = 5; // incremental change inbird_x
+float bgdt = 0.15; //0.05;
+double bgimpulse = 0;
+
+
+//if impulseMax is faster or higher in num you don't have to space bar press it as ofter
 double impulseMax = 80;
 double vyMax = 40;
 double vxMax = 40;
 
+
+//initial values for object
 double obj_x = 1000;
 double obj_y = 200;
 
 bool hasStarted = false;
 int numUpdates = 0;
+int BGUpdates = 0;
 //======================================================
-// IDLE CALLBACK ROUTINE 
+// IDLE CALLBACK ROUTINE
 //======================================================
-void bounce() {
+void bounce() {//add impulse assuming + direction
   //bird_ax += 0;
   //bird_ay += 20;
   impulse += impulseMax;
@@ -74,9 +102,166 @@ void boundImpulse() {
   if(impulse > impulseMax) impulse = 33;
   if(impulse < 0.5) impulse = 0;
 }
+//windows for buildings
+//meant to be circular looking windows
+void window(){
+    
+    for (double i =0; i <=360; i++){
+        
+    glBegin(GL_LINE_LOOP);
+    
+    glVertex3f(2.0 * cos(i),2* sin(i), 1.0);
+        glVertex3f(2.0 * cos(i+1),2* sin(i+1), 2.0);
+        glVertex3f(2.0 * cos(i),2* sin(i), 2.0);
+        glVertex3f(2.0 *cos(i+1),2* sin(i+1), 1.0);
+        
+        
+        
+        glEnd();
+    }
+}
+//Implemeted Coords for background scene
+//Used Polygons to represnt each graphic
+void background() {
+       
+   //Sky
+    glBegin(GL_POLYGON);
+          glColor3f(0.3, .9, 1);
+          glVertex2f(0, 500);
+          glVertex2f(0, 75);
+          glVertex2f(500,75);
+          glVertex2f(500, 500);
+       glEnd();
+   
+    //sidewalk
+       glBegin(GL_POLYGON);
+          glColor3f(1, 1, 0.7);
+          glVertex2f(0, 75);
+          glVertex2f(500, 75);
+          glVertex2f(500,0);
+          glVertex2f(0, 0);
+       glEnd();
+      
+    //grass
+       glBegin(GL_POLYGON);
+          glColor3f(0, 1, 0.2);
+          glVertex2f(0, 50);
+          glVertex2f(500, 50);
+          glVertex2f(500,75);
+          glVertex2f(0, 75);
+       glEnd();
+    
 
+    //clouds
+    //1
+    
+    glBegin(GL_POLYGON);
+       glColor3f(1, 1, 1);
+       glVertex2f(0, 150);
+       glVertex2f(50, 250);
+       glVertex2f(75,200);
+       glVertex2f(250, 200);
+    glEnd();
+  
+    //2
+    glBegin(GL_POLYGON);
+          glColor3f(1, 1, 1);
+          glVertex2f(0, 270);
+          glVertex2f(250, 300);
+          glVertex2f(275,270);
+          glVertex2f(450, 300);
+       glEnd();
+    
+    glFlush();
+
+    }
+    
+//Tries to for circular shapes as windows for testing purposes.
+void trees(){
+   
+    for (double i =0; i <=360; i++){
+           
+       glBegin(GL_POLYGON);
+       
+       glVertex3f(2.0 * cos(i),2* sin(i), 1.0);
+           glVertex3f(2.0 * cos(i+1),2* sin(i+1), 2.0);
+           glVertex3f(2.0 * cos(i),2* sin(i), 2.0);
+           glVertex3f(2.0 *cos(i+1),2* sin(i+1), 1.0);
+           
+           
+           
+           glEnd();
+       }
+}
+//Buildings() displays buildings in different colors widths and heights.
+void Buildings(){
+       
+    ///One -red tri shap
+    glColor3f(1, 0, 0);
+    glPushMatrix();
+    
+    glBegin(GL_POLYGON);
+    glVertex2i(200,75);
+    glVertex2i(200, 200);
+    glVertex2i(100, 75);
+    glVertex2i(100, 200);
+    
+    
+    glEnd();
+    glPopMatrix();
+
+   
+     ///purplish
+    glPushMatrix();
+    glColor3f(0.9, 0, 0.7);
+    glBegin(GL_POLYGON);
+    glVertex2i(445, 75);
+    glVertex2i(445, 200);
+    glVertex2i(350, 200);
+    glVertex2i(350, 75);
+    glEnd();
+    glPopMatrix();
+    
+    glPushMatrix();
+   
+    
+    //yellowish
+    glColor3f(0.8, 1, 0);
+       glBegin(GL_POLYGON);
+       glVertex2i(245, 75);
+       glVertex2i(245, 170);
+       glVertex2i(320, 170);
+       glVertex2i(320, 75);
+    
+    
+    glEnd();
+    glPopMatrix();
+    
+    
+     glPushMatrix();
+    //black building
+    glColor3f(0, 0.2, 0);
+       glBegin(GL_POLYGON);
+       glVertex2i(300, 75);
+       glVertex2i(300, 300);
+       glVertex2i(320, 300);
+       glVertex2i(360, 250);
+       glVertex2i(360, 75);
+    
+    
+    glEnd();
+    glPopMatrix();
+    
+    
+    }
+    
+
+
+
+//acc * dt to increase velcity, which in turns affects the position
 void update()
 {
+    
   numUpdates++;
   if(numUpdates%100==0) cout << "Num updates = " << numUpdates << "bird_x = " << bird_x << " bird_y = " << bird_y << " vx = " << bird_vx << " vy = " << bird_vy << " ax = " << bird_ax << " ay = " << bird_ay << endl;
   if(hasStarted) {
@@ -91,6 +276,8 @@ void update()
     boundVelocityAcceleration(bird_vx, bird_vy, bird_ax, bird_ay);
     impulse *= 0.3;
     boundImpulse();
+    
+     
   }
   /*
   bird_x += bird_dx; //Increment x-position of bird
@@ -108,29 +295,8 @@ void update()
     obj_y = (rand())%w_height;
   }
 
-  // Collision Check
-  float r = ((double)rand() / (RAND_MAX));
-	float g = ((double)rand() / (RAND_MAX));
-	float b = ((double)rand() / (RAND_MAX));
-	
-	if ( ((bird_x >= obj_x - 60) && (bird_x < obj_x)) && (bird_y >= obj_y + 100))
-	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(r, g, b, 1.0);
-		glutSwapBuffers();
-		glutMainLoop();
-	}
-
-	if (((bird_x >= obj_x - 60) && (bird_x < obj_x)) && (bird_y <= obj_y - 100))
-	{
-		
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(r, g, b, 1.0);
-		glutSwapBuffers();
-		glutMainLoop();
-	}
-
   glutPostRedisplay();
+   
 }
 
 //======================================================
@@ -144,7 +310,7 @@ void reshapeCallback(int width, int height)
 }
 
 //======================================================
-// MOUSE MOTION CALLBACK ROUTINE 
+// MOUSE MOTION CALLBACK ROUTINE
 //======================================================
 void motionCallBack(int x, int y)
 {
@@ -157,7 +323,7 @@ void motionCallBack(int x, int y)
 }
 
 //======================================================
-// MOUSE CALLBACK ROUTINE 
+// MOUSE CALLBACK ROUTINE
 //======================================================
 void mouseCallBack(int btn, int state, int x, int y)
 {
@@ -166,7 +332,7 @@ void mouseCallBack(int btn, int state, int x, int y)
 }
 
 //======================================================
-// KEYBOARD CALLBACK ROUTINE 
+// KEYBOARD CALLBACK ROUTINE
 //======================================================
 bool isSimulating = false;
 void keyboardCallBack(unsigned char key, int x, int y)
@@ -177,15 +343,15 @@ void keyboardCallBack(unsigned char key, int x, int y)
     case 'i':
     case 'I':
       if( !isSimulating ) {
-	isSimulating = !isSimulating;
-	printf("Idle function ON\n");
-	break;
+    isSimulating = !isSimulating;
+    printf("Idle function ON\n");
+    break;
       }
       else {
-	isSimulating = !isSimulating;
-	glutIdleFunc(NULL);
-	printf("Idle function OFF\n");
-	break;
+    isSimulating = !isSimulating;
+    glutIdleFunc(NULL);
+    printf("Idle function OFF\n");
+    break;
       }
     //case 'r':
     //  theta = theta + 10.0;
@@ -206,90 +372,44 @@ void keyboardCallBack(unsigned char key, int x, int y)
   glutPostRedisplay();
 }
 
-void DrawEllipse(float cx, float cy, float rx, float ry, int num_segments) 
-{ 
-  float theta = 2 * 3.1415926 / float(num_segments); 
+void DrawEllipse(float cx, float cy, float rx, float ry, int num_segments)
+{
+  float theta = 2 * 3.1415926 / float(num_segments);
   float c = cos(theta);//precalculate the sine and cosine
   float s = sin(theta);
   float t;
 
-  float x = 10;//we start at angle = 0 
-  float y = 0; 
+  float x = 10;//we start at angle = 0
+  float y = 0;
 
-  glBegin(GL_LINE_LOOP); 
-  for(int ii = 0; ii < num_segments; ii++) 
-  { 
+  glBegin(GL_LINE_LOOP);
+  for(int ii = 0; ii < num_segments; ii++)
+  {
     //apply radius and offset
-    glVertex2f(x * rx + cx, y * ry + cy);//output vertex 
+    glVertex2f(x * rx + cx, y * ry + cy);//output vertex
 
     //apply the rotation matrix
     t = x;
     x = c * x - s * y;
     y = s * t + c * y;
-  } 
-  glEnd(); 
+  }
+  glEnd();
 }
 
 void DrawCircle(float r, int num_segments) {
-  float theta = 2 * 3.1415926 / float(num_segments); 
+  float theta = 2 * 3.1415926 / float(num_segments);
   glBegin(GL_POLYGON);
   for(int ii=0; ii<num_segments; ii++) {
     float xi = r * cos( (theta*ii) );
     float yi = r * sin( (theta*ii) );
     glVertex2f(xi,yi);
   }
-  glEnd(); 
-}
+  glEnd();
 
-void drawPipe(float r)
-{
-  	glColor3f(0, 1, 0);
-	//Pipes
-	glBegin(GL_QUADS);
-		glVertex2f(r, r - 200);
-		glVertex2f(r + 50, r - 200);
-		glVertex2f(r + 50, r - 4200);
-		glVertex2f(r, r - 4200);
-	glEnd();
-	glBegin(GL_QUADS);
-		glVertex2f(r, r + 200);
-		glVertex2f(r + 50, r + 200);
-		glVertex2f(r + 50, r + 4200);
-		glVertex2f(r, r + 4200);
-	glEnd();
-
-	//Tip
-	glBegin(GL_QUADS);
-		glVertex2f(r - 10, r - 200);
-		glVertex2f(r + 60, r - 200);
-		glVertex2f(r + 60, r - 220);
-		glVertex2f(r - 10, r - 220);
-	glEnd();
-	glBegin(GL_QUADS);
-		glVertex2f(r - 10, r + 200);
-		glVertex2f(r + 60, r + 200);
-		glVertex2f(r + 60, r + 220);
-		glVertex2f(r - 10, r + 220);
-	glEnd();
-
-	//Black Outline
-	glColor3f(0.01, 0.01, 0.01);
-	glBegin(GL_QUADS);
-		glVertex2f(r - 10, r - 220);
-		glVertex2f(r + 60, r - 220);
-		glVertex2f(r + 60, r - 225);
-		glVertex2f(r - 10, r - 225);
-	glEnd();
-	glBegin(GL_QUADS);
-		glVertex2f(r - 10, r + 220);
-		glVertex2f(r + 60, r + 220);
-		glVertex2f(r + 60, r + 225);
-		glVertex2f(r - 10, r + 225);
-	glEnd();
 }
 
 //======================================================
-// DISPLAY CALL BACK ROUTINE 
+// DISPLAY CALL BACK ROUTINE
 //======================================================
 void displayCallBack(void)
 {
@@ -304,7 +424,7 @@ void displayCallBack(void)
     glOrtho(bird_x-w_width/2, bird_x+w_width/2, 0.0, w_height, -1.0, 1.0);
   }
 
-  // glClearColor(0.0, 0.0, 0.0, 1.0);
+  glClearColor(0.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
 
   glMatrixMode(GL_MODELVIEW);
@@ -324,20 +444,41 @@ void displayCallBack(void)
   */
   glColor3ub(255, 255, 0);
   DrawEllipse(3, 2, 3, 2, 20);
-  DrawCircle(bird_r,20);
+ 
+    
   glPopMatrix();
 
   //draw obj
-  glPushMatrix();
+  glPushMatrix();//When you call glPushMatrix you are pushing a copy of the current matrix on top of the current matrix stack; if it succeeds* there will be identical matrices at the top and just below the top of the current stack.
   glTranslatef(obj_x, obj_y, 0.0);
   glColor3f(0, 1, 0);
-  // DrawCircle(bird_r,20);
-  drawPipe(bird_r);
-  glPopMatrix();
+  //DrawCircle(bird_r,20);
+// background();
+    glColor3ub(0, 0, 250);
+    
+ 
+     glPopMatrix();
 
-  //Swap double buffers 
+     //draw obj
+     glPushMatrix();
+  
+    glTranslatef(0, 0, 0.0);
+   
+  background(); // this is the grass
+    
+  Buildings();
+  
+  glPopMatrix();// PopMatrix copies the top of the matrix into the current matrix.
+
+  //Swap double buffers
   glutSwapBuffers();
 }
+
+
+
+
+
+
 
 //======================================================
 // MAIN PROGRAM
@@ -359,11 +500,16 @@ int main(int argc, char** argv)
   glOrtho(0.0, w_width, 0.0, w_height, -1.0, 1.0);
   glMatrixMode(GL_MODELVIEW);
 
-  // Set clear color to black and clear window 
-  glClearColor(1.0, 0.0, 0.0, 1.0);
+  // Set clear color to black and clear window
+  glClearColor(0.0, 0.5, 1.0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    
   glutSwapBuffers();
 
+    
+    
   // Set up callbacks
   //glutMouseFunc(mouseCallBack);
   glutKeyboardFunc(keyboardCallBack);
@@ -374,4 +520,12 @@ int main(int argc, char** argv)
 
   // Enter main event loop
   glutMainLoop();
+     
+    
+    
+    
+    
+    
 }
+ 
+
